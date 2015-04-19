007adf2b9e9d0eea08cf23aad98f0b8b912a9087
francois53 <radior@live.fr>
2015-04-19 13:33:54 +0200

    Set to 2 seconds splash screen

--- app/src/main/java/activity/SplashScreen.java
+++ app/src/main/java/activity/SplashScreen.java
@@ -10,7 +10,7 @@ import com.example.pierre.tan.R;
 public class SplashScreen extends Activity {
 
     // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
