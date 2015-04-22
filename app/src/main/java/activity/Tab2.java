57c28b2690504bd3b5bae79bd7892e97f5891444
francois53 <radior@live.fr>
2015-04-22 20:46:09 +0200

    "Bonjour Paris"

--- app/src/main/java/activity/Tab2.java
+++ app/src/main/java/activity/Tab2.java
@@ -9,11 +9,27 @@ import android.view.ViewGroup;
 
 import com.example.pierre.tan.R;
 
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardViewNative;
+
 public class Tab2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.tab_2,container,false);
+        //Create a Card
+        Card card = new Card(getActivity());
+
+        //Create a CardHeader
+        CardHeader header = new CardHeader(getActivity());
+        header.setTitle("Bonjour Paris !");
+
+        //Add Header to card
+        card.addCardHeader(header);
+        //Set card in the cardView
+        CardViewNative cardView = (CardViewNative) v.findViewById(R.id.carddemo);
+        cardView.setCard(card);
         return v;
     }
 }
