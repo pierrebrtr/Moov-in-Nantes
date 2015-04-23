d563d890e53cb048900c0e368bb054fca4c4aae1
francois53 <radior@live.fr>
2015-04-23 20:46:31 +0200

    Add stops maps

---
+++ app/src/main/java/activity/ArretcartActivity.java
@@ -0,0 +1,76 @@
+package activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.example.pierre.tan.R;
+
+/**
+ * Created by dev on 23/04/15.
+ */
+public class ArretcartActivity extends ActionBarActivity {
+    private Toolbar mToolbar;
+    private WebView mWebView;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_arretcart);
+        final ProgressBar Pbar;
+        Pbar = (ProgressBar) findViewById(R.id.progressBar);
+        mWebView = (WebView)
+                findViewById(R.id.webview);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress)
+            {
+                if(progress < 100 && Pbar.getVisibility() == ProgressBar.GONE){
+                    Pbar.setVisibility(ProgressBar.VISIBLE);
+                }
+                Pbar.setProgress(progress);
+                if(progress == 100) {
+                    Pbar.setVisibility(ProgressBar.GONE);
+                }
+            }
+        });
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.loadUrl("https://www.google.com/maps/d/embed?mid=zXXedR5M6230.kY8yu4JPQI_o");
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        View searchContainer = findViewById(R.id.search_container);
+        final EditText toolbarSearchView = (EditText) findViewById(R.id.search);
+        ImageView searchClearButton = (ImageView) findViewById(R.id.search_clear);
+        searchClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toolbarSearchView.setText("");
+            }
+        });
+        searchContainer.setVisibility(View.GONE);
+
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+        }
+        return (super.onOptionsItemSelected(menuItem));
+    }
+
+}
